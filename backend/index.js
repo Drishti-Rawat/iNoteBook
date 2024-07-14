d99b232fcb7834474e20f5a0e5b2bf625368b4de
Drishti <drishtirwt019@gmail.com>
2024-07-14 18:34:57 +0530

    stroing data intop databases

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,22 @@ ConnectToMongo();
 
 const app = express();
 const port = 3000;
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello drishhh');
-});
+// Available routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
+// app.get('/', (req, res) => {
+//     res.send('Hello drishhh');
+// });
+// app.get('/api/v1/login', (req, res) => {
+//     res.send('Hello login');
+// });
+// app.get('/api/v1/signup', (req, res) => {
+//     res.send('Hello signup');
+// });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on http://localhost:${port}`);
 });

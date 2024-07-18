d48e158b91b1fd930994b768a791c12430f2ff3a
Drishti <drishtirwt019@gmail.com>
2024-07-19 03:16:14 +0530

    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Feature from '../components/Feature'
 import OueCustomers from '../components/OueCustomers'
-import OpenNote from '../components/OpenNote'
+
 
 const Home = () => {
   return ( 
